9c38e0b52382ec8d7e3784411620ef9d570eed44
devdazzlee <bhaia9036@gmail.com>
2026-06-12 22:04:11 +0500

    Update

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -54,6 +54,7 @@ app.use(cors({
 
     const allowedOrigins = [
       'https://yam-n7.vercel.app',
+      'https://yam-n7-dashbaord-ipo9.vercel.app',
       'https://pos.manpasandstore.com',
       'https://manpasand-pos-t623.vercel.app',
       'https://manpasand-pos-beta.vercel.app',
